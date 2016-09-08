5aa57fd1c1cef454805b2b1153399f835cdcbcfe
Zane Hooper <zanehoop@gmail.com>
2016-09-08 19:16:38 -0400

    Fix exports

--- exports.js
+++ exports.js
@@ -1,13 +1,15 @@
-module.exports = [{
-  name: 'scp-ng-bandwidth',
-  dirname: __dirname,
-  files: [
-    require.resolve('numeral/min/numeral.min.js'),
-    require.resolve('moment/moment.js'),
-    require.resolve('chart.js/dist/Chart.min.js'),
-    require.resolve('angular-chart.js/dist/angular-chart.min.js'),
-    require.resolve('bootstrap-daterangepicker/daterangepicker.js'),
-    require.resolve('bootstrap-daterangepicker/daterangepicker.css'),
-    require.resolve('angular-daterangepicker/js/angular-daterangepicker.js'),
-  ],
-}];
+var Util = require('scp-ng-util');
+
+module.exports = [
+  Util
+    .export('scp-ng-bandwidth', __dirname)
+    .node([
+      'numeral/min/numeral.min.js',
+      'moment/moment.js',
+      'chart.js/dist/Chart.min.js',
+      'angular-chart.js/dist/angular-chart.min.js',
+      'bootstrap-daterangepicker/daterangepicker.js',
+      'bootstrap-daterangepicker/daterangepicker.css',
+      'angular-daterangepicker/js/angular-daterangepicker.js',
+    ]),
+];
