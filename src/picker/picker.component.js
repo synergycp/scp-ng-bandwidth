940819cabfa0537ba0080303f1f694b6efec3aaf
Андрей Михацкий <andrey.mikhatskiy@gmail.com>
2017-06-28 14:00:34 +0300

    added select to increment/decrement filter ranges

--- src/picker/picker.component.js
+++ src/picker/picker.component.js
@@ -1,6 +1,25 @@
 (function () {
   'use strict';
 
+  var SHIFTS = [
+    {
+      label: '5 minutes',
+      value: [5, 'minutes']
+    }, {
+      label: '30 minutes',
+      value: [30, 'minutes']
+    }, {
+      label: '2 hours',
+      value: [2, 'hours']
+    }, {
+      label: '6 hours',
+      value: [6, 'hours']
+    }, {
+      label: '1 day',
+      value: [1, 'day']
+    }
+  ]
+
   angular
     .module('scp.bandwidth.picker')
     .component('bandwidthDateRangePicker', {
@@ -30,11 +49,15 @@
   /**
    * @ngInject
    */
-  function BandwidthDateRangePickerCtrl($element, $timeout) {
+  function BandwidthDateRangePickerCtrl($element, $timeout, moment) {
     var picker = this;
 
     picker.$onInit = init;
     picker.openFilter = openFilter;
+    picker.changeRange = changeRange;
+    picker.selected = null;
+    picker.rangeShifts = _.clone(SHIFTS);
+    picker.selectedRangeShift = picker.rangeShifts[0];
 
     //////////
 
@@ -55,6 +78,15 @@
 
     function $picker() {
       return $('.date-picker', $element[0]);
+    }
+
+    function changeRange(incOrDec, value) {
+      var start = moment(picker.bandwidth.filter.start);
+      var end = moment(picker.bandwidth.filter.end);
+      var method = (incOrDec == 'inc' && 'add') || (incOrDec == 'dec' && 'subtract');
+      var newStart = start[method](value[0], value[1]);
+      var newEnd = end[method](value[0], value[1]);
+      picker.bandwidth.filter.setRange(newStart, newEnd)
     }
   }
 })();
